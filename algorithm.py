0fe0b9898fdb0a5cb5ba336b4d6e0e01ef35fa64
Tony Yang <tyang27@jhu.edu>
2019-02-03 11:19:30 -0500

    able to add one slot

---
+++ algorithm.py
@@ -0,0 +1,129 @@
+import numpy as np
+import pandas as pd
+
+class Day:
+    def __init__(self, ppl=None):
+        if ppl is None:
+            self.ppl = {}
+        else:
+            self.ppl = ppl
+    
+    def add_appt(self, ID, time):
+        if self.check_appt(time) == -1:
+            self.ppl[time] = ID
+    
+    def check_appt(self, time):
+        if time in self.ppl:
+            return self.ppl[time]
+        return -1
+    
+    def remove_appt(self, time):
+        if self.check_appt(time) != -1:
+            self.ppl.pop(time)
+    
+    def to_string(self):
+        output = ""
+        for key in self.ppl:
+            output = output + str(key) + " " + str(self.ppl[key]) + "\n"
+        return output
+            
+class Week:
+    def __init__(self, days=None, patient_data=None, possible_times=None):
+        if days is None:
+            self.days = {"Monday": Day(), "Tuesday": Day(), "Wednesday": Day(), "Thursday": Day(), "Friday": Day()}
+        else:
+            self.days = days
+        
+        if possible_times is None:
+            self.possible_times = []
+            for i in range(900, 1700, 100):
+                for j in range(0, 60, 15):
+                    self.possible_times.append(i + j)
+        else:
+            self.possible_times = possible_times
+        print(self.possible_times)
+        
+    def add_appt(self, day, ID, time):
+        if day in self.days:
+            self.days[day].add_appt(ID, time)
+    
+    def check_appt(self, day, time):
+        if day in self.days:
+            return self.days[day].check_appt(time)
+        return -2
+    
+    def cancel_appt(self, day, time):
+        if self.check_appt(day, time) > -1:
+            self.days[day].remove_appt(time)
+            
+    def to_string(self):
+        output = ""
+        daysofweek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+        for key in daysofweek:
+            output = output + str(key) + ":\n" + self.days[key].to_string() + "\n"
+        return output
+
+    def __str__(self):
+        return self.to_string();
+
+def add_time(x, y):
+    z = x + y
+
+    if z - 100*(z % 100) >= 60:
+        z = z + 40
+    return z
+
+def overlap_calculator(week, day, time):
+    if add_time(time, 15) in week.days[day]:
+        return 15
+    return 0
+
+def idleness_cost(week, day, ID, time, data):
+    week.add_appt(day, ID, time)
+    cost = 0
+    for keys in week.days[day]:
+        cost += (30-overlap_calculator(week, day, keys))* data.loc(ID, "Cancellation Index")
+    week.cancel_appt(day, ID, time)
+    return cost
+
+def waiting_cost(week, day, ID, time, data):
+    week.add_appt(day, ID, time)
+    cost = 0
+    for time in self.possible_times:
+        cost += (overlap_calculator(week, day, keys))*(1-data.loc(ID, "Cancellation Index"))
+    week.cancel_appt(day, ID, time)
+    return cost
+
+def cost(week, day, ID, time, data):
+    beta = 1
+    gamme = 1
+    return beta*idleness_cost(week, day, ID, time, data) + gamma*waiting_cost(week, day, ID, time, data)
+
+def schedule_appt(week, day, ID, data):
+    min_cost = -1
+    min_appt_time = 0
+    appt_time = 0
+    while(appt_time < len(week.possible_times)):
+        if week.check_appt(day, week.possible_times[appt_time]) < 0:
+            appt_time += 1
+        else:
+            cur_cost = cost(week, day, ID, week.possible_times[appt_time], data)
+            if min_cost == -1:
+                min_appt_time = appt_time
+                min_cost = cur_cost
+            elif cur_cost < min_cost:
+                min_appt_time = appt_time
+                min_cost = cur_cost
+            appt_time += 1
+    week.add_appt(day, ID, week.possible_times[min_appt_time])
+
+def complete_appt(week, day, time, noshow):
+    pat_id = week.check_appt(day, time)
+
+    if pat_id > 0:
+        patient_info = patient_roster.loc[[ID]]
+        patient_data = pd.merge(patient_data, patient_info)
+
+        patient_roster.at[ID, "Previous No-Show Coefficient"] = 0.8 * noshow + 0.2 * patient_roster.at[ID, "Previous No-Show Coefficient"]
+
+        week.cancel_appt(day, time)
--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, url_for, request, flash, Session
 from datetime import datetime
+import pandas as pd
 import os
+
+import algorithm as sch
+from algorithm import Week, Day
 
 UPLOADS = '/uploads'
 ALLOWED_EXTENSIONS = set(['txt', 'csv'])
-
 INCREMENTS = [
     '09:00', '09:15', '09:30', '09:45',
     '10:00', '10:15', '10:30', '10:45',
@@ -16,28 +19,40 @@ INCREMENTS = [
     '16:00', '16:15', '16:30', '16:45',
     '17:00'
     ]
+SCHEDULE = []
 
+# FLASK APP
 app = Flask(__name__)
 app.config['SESSION_TYPE'] = 'memcached'
 app.config['SECRET_KEY'] = 'super secret key'
 sess = Session()
-#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# BACKEND DATA
+week = sch.Week()
+patient_roster = pd.read_csv("data/patient_roster.csv")
+patient_data = pd.read_csv("data/patient_data.csv")
 
-@app.route('/upload', methods=['GET', 'POST'])
-def upload_data():
-    wf='No input detected'
-    if request.method == 'POST':
-        print('POST')
-        wf = request.form['waveform']
-    return render_template('results.html', waveform = wf)
+# Construct an inflatable table from Week object
+dayofweek_translate = {
+        "Monday":2,
+        "Tuesday":3,
+        "Wednesday":4,
+        "Thursday":5,
+        "Friday":6
+        }
+def time_translate(military_time):
+    return int((military_time - 900) / 100)
 
+def build_week(week):
+    SCHEDULE = []
+    for dayofweek, day in week.days.items():
+        for pat_time, pat_id in day.ppl.items():
+            SCHEDULE.append((pat_id, time_translate(pat_time), dayofweek_translate[dayofweek]))
+    return SCHEDULE
 
 @app.route('/incoming', methods=['GET','POST'])
 def incoming():
+    """Handle scheduling for new user and re-render home page."""
     if request.method == 'POST':
         print('POST')
         print(request.form)
@@ -45,45 +60,46 @@ def incoming():
             print('empty field')
         elif request.form['day'] == '':
             print('empty field')
-    return render_template('index.html', increments=INCREMENTS)
+    return render_template('index.html', increments=INCREMENTS, time=SCHEDULE)
 
 @app.route('/returning', methods=['GET','POST'])
 def returning():
+    """Handle scheduling for return user and re-render home page."""
     if request.method == 'POST':
         print('POST')
         print(request.form)
-        if request.form['id'] == '':
+
+        if request.form['id'] == '' or request.form['week'] == '' or request.form['day'] == '':
             print('empty field')
-        elif request.form['week'] == '':
-            print('empty field')
-        elif request.form['day'] == '':
-            print('empty field')
-    return render_template('index.html', increments=INCREMENTS)
+            return render_template('index.html', increments=INCREMENTS)
+
+        sch.schedule_appt(week, request.form['day'], request.form['id'], patient_roster)
+
+    return render_template('index.html', increments=INCREMENTS, time=build_week(week))
 
 @app.route('/checkin', methods=['GET', 'POST'])
 def checkin():
+    """Handle checkout form submission and re-render home page."""
     if request.method == 'POST':
-        print('Checkin post')
+        username = request.form['username']
+        time = request.form['time'] if request.form['time'] != '' else datetime.now().strftime('%H:%M')
         print(request.form)
-    username = request.form['username']
-    print(username)
-    time = request.form['time'] if request.form['time'] != '' else datetime.now().strftime('%H:%M')
-    return render_template('checkin.html',
-        username=username,
-        time=time)
+    return render_template('index.html', increments=INCREMENTS, time=SCHEDULE)
 
 @app.route('/checkout', methods=['GET', 'POST'])
 def checkout():
+    """Handle checkout form submission and re-render home page."""
     if request.method == 'POST':
-        print('Checkout post')
+        username = request.form['username']
+        time = request.form['time'] if request.form['time'] != '' else datetime.now().strftime('%H:%M')
         print(request.form)
-    username = request.form['username']
-    print(username)
-    time = request.form['time'] if request.form['time'] != '' else datetime.now().strftime('%H:%M')
-    return render_template('checkout.html',
-        username=username,
-        time=time)
+    return render_template('index.html', increments=INCREMENTS, time=SCHEDULE)
 
+@app.route('/')
+def index():
+    """Render home page."""
+    # (day of week, start time, user id, probability))
+    return render_template('index.html', increments=INCREMENTS, time=SCHEDULE)
 
 @app.context_processor
 def override_url_for():
@@ -97,11 +113,6 @@ def dated_url_for(endpoint, **values):
                                  endpoint, filename)
             values['q'] = int(os.stat(file_path).st_mtime)
     return url_for(endpoint, **values)
-
-@app.route('/')
-def index():
-    """Render home page."""
-    return render_template('index.html', increments=INCREMENTS)
 
 if __name__ == '__main__':
     app.run()
